7ac5c8561acc4436fc955953df51a809ced2ac97
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 20:09:42 +0700

    add data class HomeUiState

--- app/src/main/java/com/example/pam_act7/model/HomeViewModel.kt
+++ app/src/main/java/com/example/pam_act7/model/HomeViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.pam_act7.model
 
 import androidx.lifecycle.ViewModel
+import com.example.pam_act7.data.Siswa
 import com.example.pam_act7.repositori.RepositoriSiswa
+
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
