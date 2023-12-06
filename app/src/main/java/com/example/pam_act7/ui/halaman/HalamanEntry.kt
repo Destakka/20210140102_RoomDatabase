159d70f9592246543ccb02f15b3cb225665ec2ef
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 21:41:09 +0700

    add object DestinasiEntry

---
+++ app/src/main/java/com/example/pam_act7/ui/halaman/HalamanEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pam_act7.ui.halaman
+
+import com.example.pam_act7.R
+import com.example.pam_act7.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
