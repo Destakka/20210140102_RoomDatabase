1c55109ce68cd5a746b12d7c6586f9fafabe9fbe
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 19:15:47 +0700

    add class EntryViewModel

---
+++ app/src/main/java/com/example/pam_act7/model/EntryViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.pam_act7.model
+
+import androidx.lifecycle.ViewModel
+import com.example.pam_act7.repositori.OfflineRepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: OfflineRepositoriSiswa): ViewModel() {
+}
