8a0e9665862b5374115ceb659542ff4e9c7b3fef
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 20:05:49 +0700

    add class HomeViewModel

---
+++ app/src/main/java/com/example/pam_act7/model/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.pam_act7.model
+
+import androidx.lifecycle.ViewModel
+import com.example.pam_act7.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+}
