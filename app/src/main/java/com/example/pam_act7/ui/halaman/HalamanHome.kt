673c6cbcd38c218b20f4baa6ed02f5e5754d9791
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 23:02:16 +0700

    Tambahkan Column untuk menampilkan teks atau daftar siswa

--- app/src/main/java/com/example/pam_act7/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pam_act7/ui/halaman/HalamanHome.kt
@@ -14,8 +14,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.pam_act7.R
 import com.example.pam_act7.data.Siswa
@@ -30,8 +33,25 @@ object DestinasiHome : DestinasiNavigasi {
 fun BodyHome(
     itemSiswa: List<Siswa>,
     modifier: Modifier = Modifier
-){
-
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemSiswa.isEmpty()) {
+            Text(
+                text = stringResource(id = R.string.deskripsi_no_item),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        } else {
+            ListSiswa(
+                itemSiswa = itemSiswa,
+                modifier = Modifier
+                    .padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
 }
 
 @Composable
