877de5541c9bcdc55e17acb73fa4cb400d9fb992
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 19:21:28 +0700

    add dataclass UIStateSiswa

--- app/src/main/java/com/example/pam_act7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/pam_act7/model/EntryViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.pam_act7.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.pam_act7.repositori.OfflineRepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: OfflineRepositoriSiswa): ViewModel() {
 }
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
