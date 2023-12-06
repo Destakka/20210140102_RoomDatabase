b67c9b8ba6459a61ab8d5caa3032bf8d1ec62612
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 19:44:04 +0700

    add function siswa UIStateSiswa

--- app/src/main/java/com/example/pam_act7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/pam_act7/model/EntryViewModel.kt
@@ -45,12 +45,20 @@ data class DetailSiswa(
     val alamat: String = "",
     val telpon: String = ""
 )
+
+/* Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya*/
 fun DetailSiswa.toSiswa(): Siswa = Siswa(
     id = id,
     nama = nama,
     alamat = alamat,
     telpon = telpon
 )
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
 
 
 
