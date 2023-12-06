d96263059b4b699e3b896e38d9868081d46e1cf9
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 22:55:25 +0700

    add column dg padding dan vertical arragement

--- app/src/main/java/com/example/pam_act7/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pam_act7/ui/halaman/HalamanHome.kt
@@ -1,9 +1,14 @@
 package com.example.pam_act7.ui.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
 import com.example.pam_act7.R
 import com.example.pam_act7.data.Siswa
 import com.example.pam_act7.navigasi.DestinasiNavigasi
@@ -23,6 +28,12 @@ fun DataSiswa(
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     )
     {
-        
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        )
+        {
+
+        }
     }
 }
