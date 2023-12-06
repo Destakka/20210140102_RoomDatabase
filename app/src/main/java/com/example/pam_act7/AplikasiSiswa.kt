078e8ed429998babcbafc0e89929d753c2b138d6
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 20:17:45 +0700

    Add lateinit declaration for 'container: ContainerApp

--- app/src/main/java/com/example/pam_act7/AplikasiSiswa.kt
+++ app/src/main/java/com/example/pam_act7/AplikasiSiswa.kt
@@ -1,4 +1,12 @@
 package com.example.pam_act7
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.pam_act7.repositori.ContainerApp
+
+class AplikasiSiswa : Application(){
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
 }
