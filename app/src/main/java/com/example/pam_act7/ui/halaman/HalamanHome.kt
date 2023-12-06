47ee95ed0ce23eb02eadb461d85927aa9995c5a5
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 22:58:54 +0700

    add text untuk menampilkan alamat siswa

--- app/src/main/java/com/example/pam_act7/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pam_act7/ui/halaman/HalamanHome.kt
@@ -58,7 +58,10 @@ fun DataSiswa(
                     style = MaterialTheme.typography.titleMedium
                 )
             }
-
+            Text(
+                text = siswa.alamat,
+                style = MaterialTheme.typography.titleMedium
+            )
         }
     }
 }
