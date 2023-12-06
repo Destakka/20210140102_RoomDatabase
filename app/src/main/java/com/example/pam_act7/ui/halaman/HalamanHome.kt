e9e419c459ba1391a46528abf73e262a4d4bddc5
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 22:53:12 +0700

    add function DataSiswa untuk menampilkan data siswa

--- app/src/main/java/com/example/pam_act7/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pam_act7/ui/halaman/HalamanHome.kt
@@ -1,8 +1,20 @@
 package com.example.pam_act7.ui.halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pam_act7.R
+import com.example.pam_act7.data.Siswa
 import com.example.pam_act7.navigasi.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
+}
+
+@Composable
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+){
+
 }
