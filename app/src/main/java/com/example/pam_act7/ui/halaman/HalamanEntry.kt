560ae45d2e9da9999ddeec788bb01836d3f5f74a
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-12-06 22:17:37 +0700

    add komponen OutlinedTextFiled untuk input nama

--- app/src/main/java/com/example/pam_act7/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pam_act7/ui/halaman/HalamanEntry.kt
@@ -2,12 +2,16 @@ package com.example.pam_act7.ui.halaman
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.room.util.TableInfo
 import com.example.pam_act7.R
 import com.example.pam_act7.model.DetailSiswa
@@ -52,6 +56,14 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ){
-        
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange = {onValueChange(detailSiswa.copy(nama = it))},
+            label = { Text(stringResource(id = R.string.nama))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
     }
 }
